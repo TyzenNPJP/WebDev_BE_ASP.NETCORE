f94d8590325c09082b2e0cf7a2ec7da40a16b888
Tyzen <taizanlama.edu@gmail.com>
2023-07-18 23:16:18 +0545

    DNC1 - HTTP - Request / Response

---
+++ DNC1_HTTP-Request-Response/CODE_Program.cs
@@ -0,0 +1,25 @@
+/*
+ *  ABOUT - HTTP REQUEST / HTTP RESPONSE
+ * 
+ * KEYWORDS - WebApplication, CreateBuilder(), Build(), Run(), HttpContext, async
+ *              , Request, Method, GET, Path, Query, ContainsKey, WriteAsync("").
+*/
+
+var builder = WebApplication.CreateBuilder();
+var app = builder.Build();
+
+
+app.Run(async (HttpContext context) =>
+{
+    if (context.Request.Method == "GET" && context.Request.Path == "/")
+    {
+        if (context.Request.Query.ContainsKey("q"))
+        {
+            string q_str = context.Request.Query["q"][0];
+
+            await context.Response.WriteAsync(q_str);
+        }
+    }
+});
+
+app.Run();
