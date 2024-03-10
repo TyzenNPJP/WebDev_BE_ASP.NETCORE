aab6786c9c137ec968ae7a53e51c2776c66afc11
Tyzen <taizanlama.edu@gmail.com>
2024-03-11 08:54:42 +1100

    DNC1 - HTTP Request & Response

--- DNC1_HTTP-Request-Response/obj/Debug/net6.0/DNC1_HTTP-Request-Response.AssemblyInfo.cs
+++ DNC1_HTTP-Request-Response/obj/Debug/net6.0/DNC1_HTTP-Request-Response.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DNC1_HTTP-Request-Response")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81cbe76664a7504caf7d03c163c10cccb03eab73")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+576b5c0b7e5ee2fac94e8ab02c0c5767ed290dfa")]
 [assembly: System.Reflection.AssemblyProductAttribute("DNC1_HTTP-Request-Response")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DNC1_HTTP-Request-Response")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
